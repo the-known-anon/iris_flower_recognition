319129d92f004473aac95fc6e84c1b918e12f4c2
the-known-anon <56070522+the-known-anon@users.noreply.github.com>
2020-10-02 13:59:12 +0530

    added comments

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+# import all the libraries
 import sys
 import pandas as pd
 import matplotlib 
@@ -9,9 +10,10 @@ import mglearn
 import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
 from sklearn.neighbors import KNeighborsClassifier
-
+from sklearn.model_selection import train_test_split
 
 from sklearn.datasets import load_iris
+#load the data set
 iris_dataset= load_iris()
 
 print ("keys of iris dataset would be \n{}".format(iris_dataset.keys()))
@@ -19,7 +21,7 @@ print("Target names: {}".format(iris_dataset['target_names']))
 
 print("Feature names: \n{}".format(iris_dataset['feature_names']))
 
-from sklearn.model_selection import train_test_split
+#Now we will split the dataset, into test and train datasets
 X_train,X_test,Y_train,Y_test = train_test_split(iris_dataset['data'],iris_dataset['target'],random_state=0)
 
 print("X_train shape: {}".format(X_train.shape))
@@ -29,6 +31,7 @@ iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
 # create a scatter matrix from the dataframe, color by y_train
 grr = pd.plotting.scatter_matrix(iris_dataframe, c=Y_train, figsize=(15, 15), marker='o',
 hist_kwds={'bins': 20}, s=60, alpha=.8)
+#We will be using the KNN model for training on the dataset
 knn = KNeighborsClassifier(n_neighbors = 2)
 knn.fit(X_train,Y_train)
 X_new = np.array([[8, 3, 5, 2]])
@@ -40,6 +43,7 @@ iris_dataset['target_names'][prediction]))
 y_pred = knn.predict(X_test)
 print("{}".format(knn.score(X_test,Y_test)))
 X,y= mglearn.datasets.make_wave(n_samples = 40)
+#Plot the test data, according to the predictions made
 plt.plot(X,y,'')
 
 
